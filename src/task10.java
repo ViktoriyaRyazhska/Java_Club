e07f951f7da01d13e4d5524f6c576bdea90726bc
Ihor <volyanskiy.i@gmail.com>
2021-07-08 18:03:54 +0300

    Added interactive part to existing tasks

--- src/task10.java
+++ src/task10.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class task10 {
     public static String datingRange(int age) {
         int minAge;
@@ -14,4 +16,12 @@ public class task10 {
     }
 
 
+    public static void execute(Scanner sc) {
+        System.out.println("Everybody knows the classic \"half your age plus seven\" dating rule \nthat a lot of people follow (including myself).\n" +
+                "It's the 'recommended' age range in which to date someone.");
+        System.out.println("Please enter your age: ");
+        int n = sc.nextInt();
+        String result = datingRange(n);
+        System.out.println("Your partner age should be from/till " + result);
+    }
 }
