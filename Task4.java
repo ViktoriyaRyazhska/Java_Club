1989fd46152d4dcfe22f50c722eff45750d000e1
Vasiuta <vstoleksiy@gmail.com>
2021-07-10 16:49:48 +0300

    Team4Task4

---
+++ Task4.java
@@ -0,0 +1,11 @@
+//Task 4 (Thinkful - Number Drills: Pixelart planning)
+
+public class Kata {
+
+  public static boolean isDivisible(int wallLength, int pixelSize) {
+    if(wallLength%pixelSize==0)
+      return true;
+    return false;
+  }
+
+}
