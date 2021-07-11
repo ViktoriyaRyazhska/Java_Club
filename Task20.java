8736858c41c6a6ba8476b9c24832c6dda882e110
Vasiuta <vstoleksiy@gmail.com>
2021-07-11 18:22:17 +0300

    Team4Task20

---
+++ Task20.java
@@ -0,0 +1,12 @@
+//Task 20 (Convert a Boolean to a String)
+
+public class BooleanToString {
+  public static String convert(boolean b){
+    if(b == true) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+
+}
