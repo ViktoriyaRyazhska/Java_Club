5bb2c892c9fe6a933cb8ba7540c5d8394382281a
Taras <mythbuster.lviv@gmail.com>
2021-07-12 16:42:47 +0300

    Team1 basic task and UI menu

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import com.company.basicTasks.BasicMenu;
+import com.company.basicTasks.BasicTask1;
+import com.company.basicTasks.BasicTask2;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        //System.out.println(datingRange(12));
+
+
+
+
+
+//        BasicMenu basicMenu = new BasicMenu();
+//
+//        int task = -1;
+//        boolean mark = false;
+//        while (true) {
+//            basicMenu.printListOfTasks();
+//            do {
+//                try {
+//                    Scanner sc = new Scanner(System.in);
+//                    task = sc.nextInt();
+//
+//                    mark = true;
+//                } catch (Exception exception) {
+//                    System.out.println("You have input illegal character, try again!");
+//
+//                }
+//            } while (mark == false);
+//            basicMenu.menu(task);
+//
+//
+//        }
+    }
+}
