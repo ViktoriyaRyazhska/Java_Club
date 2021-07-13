cebb9df020278cea3d4441e51888f2ba2b2f60db
Ihor <volyanskiy.i@gmail.com>
2021-07-13 22:06:35 +0300

    Added interactive part to task36

--- src/task36.java
+++ src/task36.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class task36 {
   public int countSheeps(Boolean[] arrayOfSheeps) {
     int result = 0;
@@ -6,5 +8,27 @@ public class task36 {
         result++;
     }
     return result;
+  }
+
+  public static void execute(Scanner sc) {
+    System.out.println("Consider an array/list of sheep where some sheep may " +
+            "be missing from their place. \n" +
+            "This program counts the number of sheep present in the array (true means present).");
+    System.out.println("Enter size of array: ");
+    int size = sc.nextInt();
+    Boolean[] sheep = new Boolean[size];
+    for (int i = 0; i < size; i++) {
+      System.out.println("Enter true or false: ");
+      String str = sc.next().toLowerCase();
+      if (str.equals("true")) {
+        sheep[i] = true;
+      } else if (str.equals("false")) {
+        sheep[i] = false;
+      } else {
+        throw new IllegalArgumentException("Wrong value!!!");
+      }
+    }
+    int result = new task36().countSheeps(sheep);
+    System.out.println("There is/are " + result + " sheep");
   }
 }
