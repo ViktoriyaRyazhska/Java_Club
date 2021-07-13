668900d2368315aea98bbd5abbdd821fce4b8c14
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-13 17:14:42 +0300

    add Task 3 Conditions Convert bool to String

---
+++ src/main/java/com/team1/tasks/conditionsTasks/ConvertBoolToString.java
@@ -0,0 +1,11 @@
+package com.team1.tasks.conditionsTasks;
+
+public class ConvertBoolToString {
+    public static String convert(boolean b) {
+        if (b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
