a2269edc99a983a217d2bb080b34934135840e26
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-14 12:38:55 +0300

    add Task 1 OOP Basic subclasses: Adam and Eve

---
+++ src/main/java/com/team1/tasks/OOP/subclasses/Man.java
@@ -0,0 +1,9 @@
+package com.team1.tasks.OOP.subclasses;
+
+public class Man extends Human {
+    private final String line = "Adam is a man";
+
+    public Man() {
+        System.out.println(line);
+    }
+}
