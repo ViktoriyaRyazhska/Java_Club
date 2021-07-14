64f6315c6a1767d0e35a30339b5c7adfbe9caccc
Vasiuta <vstoleksiy@gmail.com>
2021-07-14 23:43:38 +0300

    team4

---
+++ src/main/java/com/team4/MyMain.java
@@ -0,0 +1,58 @@
+package com.team4;
+
+import java.util.Scanner;
+
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.print("Input your number of task : ");
+        Scanner scanner = new Scanner(System.in);
+        int question = scanner.nextInt();
+        switch (question){
+            case 1:
+                System.out.println("Input your number ");
+                scanner = new Scanner(System.in);
+                int n = scanner.nextInt();
+                System.out.println(Java.doubleInteger(n));
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+
+
+
+
+
+        }
+    }
+}
--- src/main/java/com/team4/Task1.java
+++ src/main/java/com/team4/Task1.java
@@ -1,5 +1,5 @@
 //Task 1 (You Can't Code Under Pressure #1)
-
+package com.team4;
 class Java {
   public static int doubleInteger(int i) {
     i *= 2;
