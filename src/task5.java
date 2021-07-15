47a38e5df6f1a57f284e755862c3cb3898918172
Ihor Volianskyi <volyanskiy.i@gmail.com>
2021-07-15 22:32:31 +0300

    Added interactive part to task5

--- src/task5.java
+++ src/task5.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class task5 {
-    public static void main(String[] args) {
-
-    }
 
     public static String greet(String name) {
         if (!name.equals("Johnny")) {
@@ -15,7 +12,9 @@ public class task5 {
     }
     public static void execute(Scanner sc) {
         System.out.println("This program greets a person ");
-
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        System.out.println(greet(name));
     }
 
 }
