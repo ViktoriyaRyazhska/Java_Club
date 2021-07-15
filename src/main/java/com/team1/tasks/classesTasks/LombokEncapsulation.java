68beb9e2774323d004d9502c1767a933773e6129
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-16 00:03:04 +0300

    add toString method,realised execute method, delete adObject

--- src/main/java/com/team1/tasks/classesTasks/LombokEncapsulation.java
+++ src/main/java/com/team1/tasks/classesTasks/LombokEncapsulation.java
@@ -9,13 +9,24 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class LombokEncapsulation implements IRunTask {
+public class LombokEncapsulation implements IRunTask  {
     public int number;
     public String stringValue;
-    public Object anObject;
+
+    @Override
+    public String toString() {
+        return "LombokEncapsulation{" +
+                "number=" + number +
+                ", stringValue='" + stringValue + '\'' +
+                '}';
+    }
 
     @Override
     public void execute() {
-
+        LombokEncapsulation lombokEncapsulation=new LombokEncapsulation();
+        LombokEncapsulation lombokEncapsulation2=new LombokEncapsulation(2,"lombok is nice)");
+        lombokEncapsulation.setNumber(1);
+        lombokEncapsulation.setStringValue("lombok is cool)");
+        System.out.println(lombokEncapsulation+"\n"+lombokEncapsulation2);
     }
 }
