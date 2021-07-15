4e484409fd8e8ae780650682c54b63b00d7f5eb1
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-15 13:26:49 +0300

    add Task 6 Conditions Rock Paper scissors

---
+++ src/main/java/com/team1/tasks/conditionsTasks/RockPaperScissors.java
@@ -0,0 +1,19 @@
+package com.team1.tasks.conditionsTasks;
+
+public class RockPaperScissors {
+    private RockPaperScissors() {
+    }
+
+    public static String rps(String p1, String p2) {
+        // your code
+        if ((p1.equals("scissors") && p2.equals("paper"))
+                || (p1.equals("rock") && p2.equals("scissors"))
+                || (p1.equals("paper") && p2.equals("rock"))) {
+            return "Player 1 won!";
+        } else if (p1.equals(p2)) {
+            return "Draw!";
+        } else {
+            return "Player 2 won!";
+        }
+    }
+}
