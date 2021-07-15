2888063c15b3919c42ce93bd5543362833aa61df
Taras <mythbuster.lviv@gmail.com>
2021-07-15 23:14:22 +0300

    test

--- src/com/team1/tasks/collections/InteractiveDictionary.java
+++ src/com/team1/tasks/collections/InteractiveDictionary.java
@@ -26,7 +26,32 @@ public class InteractiveDictionary implements IRunTask {
 
     @Override
     public void execute() {
-
+        boolean mark = false;
+        System.out.println("Input to integer temperature you want to convert to fahrenheit !");
+        do {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Input amount of words you want to add to the dictionary");
+                int size = sc.nextInt();
+                for (int i = 0; i <size; i++) {
+                    System.out.println("Input word");
+                    String key = sc.next();
+                    System.out.println("Input description");
+                    String value = sc.next();
+                    newEntry(key,value);
+                }
+                while (true){
+                    System.out.println("Input word you want to get description, press 0 to stop the program");
+                    String key = sc.next();
+                    look(key);
+                    if (key =="0"){break;}
+                }
+                mark = true;
+            } catch (Exception exception) {
+                System.out.println("You have input illegal character, try again!");
+                continue;
+            }
+        } while (mark == false);
     }
 }
 
