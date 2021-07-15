fd5ab55914ead3452fe8e59a18d6986d1db1e047
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-15 23:36:19 +0300

    minor changes

--- src/main/java/com/team1/Main.java
+++ src/main/java/com/team1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BasicMenu basicMenu = new BasicMenu();
+        /*BasicMenu basicMenu = new BasicMenu();
 
         int task = -1;
         boolean mark = false;
@@ -27,6 +27,6 @@ public class Main {
             basicMenu.menu(task);
 
 
-        }
+        }*/
     }
 }
