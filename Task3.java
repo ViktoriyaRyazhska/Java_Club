1466ec88f8bbca4ffed4c454e0a0565e97404254
Yura Nesterovych <yranest2001@gmail.com>
2021-07-17 15:01:57 +0300

    delete

--- Task3.java
+++
@@ -1,5 +0,0 @@
-class Kata {
-    public int  getVolumeOfCuboid(int length, int width, int height) {
-        return  length*width*height;
-    }
-}
--- Task7.java
+++
@@ -1,5 +0,0 @@
-class Task7{
-    public int past(int h,int m,int s){
-        return h*3600000+m*60000+s*1000;
-    }
-}
