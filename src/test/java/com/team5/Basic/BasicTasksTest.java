1c9f00959822482bb6414b7bdb9c3743b130e39b
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-07-18 00:07:28 +0300

    Junit test

---
+++ src/test/java/com/team5/Basic/BasicTasksTest.java
@@ -0,0 +1,59 @@
+package com.team5.Basic;
+
+import com.team5.Menu.Input;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicTasksTest {
+
+    BasicTasks basicTask = new BasicTasks();
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void task1() {
+
+    }
+
+    @Test
+    void task2() {
+
+    }
+
+    @Test
+    void task3() {
+    }
+
+    @Test
+    void task4() {
+    }
+
+    @Test
+    void task5() {
+    }
+
+    @Test
+    void task6() {
+    }
+
+    @Test
+    void task7() {
+    }
+
+    @Test
+    void task8() {
+    }
+
+    @Test
+    void task9() {
+    }
+}
