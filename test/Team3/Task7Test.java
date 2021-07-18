0ce3ed9d5bc9a0f917af236098d370e4d4884da2
Volodymyr <2vovik4@gmail.com>
2021-07-18 19:41:37 +0300

    add

--- test/Team3/Task7Test.java
+++ test/Team3/Task7Test.java
@@ -1,4 +1,18 @@
+package Team3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Task7Test {
-  
+
+    @Test
+    public void past() {
+        int actual = Task7.past(2,15,45);
+        int expected = 8145000;
+        int delta = 0;
+        Assert.assertEquals(expected,actual,delta);
+    }
+
 }
