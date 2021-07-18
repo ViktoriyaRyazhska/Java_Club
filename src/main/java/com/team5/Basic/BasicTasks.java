a3b24d1711ed2567ec0dd177457f014e50983752
AVykhovanok <A.Vykhovanok@gmail.com>
2021-07-18 23:30:59 +0300

    oles

--- src/main/java/com/team5/Basic/BasicTasks.java
+++ src/main/java/com/team5/Basic/BasicTasks.java
@@ -45,92 +45,91 @@ public class BasicTasks {
         System.out.println(task3(length,width,height));
     }
 
-    public boolean task4(){
+        public boolean task4 ( int a, int b){
+            return a % b == 0;
+        }
+        public void task4IO () {
+            System.out.println("Enter size of the wall in millimeters");
+            int a = Input.getIntegerInput();
+            System.out.println("Enter size of a pixel in millimeters");
+            int b = Input.getIntegerInput();
+            System.out.println(a % b == 0);
+            System.out.println(task4(a, b));
 
-    public boolean task4(int a,int b){
-        return a%b==0;
-    }
-    public void task4IO(){
-        System.out.println("Enter size of the wall in millimeters");
-        int a = Input.getIntegerInput();
-        System.out.println("Enter size of a pixel in millimeters");
-        int b = Input.getIntegerInput();
-        System.out.println(a%b==0);
-        System.out.println(task4(a,b));
-
-    }
-
-    public String task5 (String name) {
-        if(name.equals("Johnny")){
-            return "Hello, my love!";
-        }else{
-            return String.format("Hello, %s!", name);
         }
 
-    }
-    public void task5IO () {
-        System.out.println("Enter your name");
-        String name = Input.getStrInput();
-        System.out.println(task5(name));
+        public String task5 (String name){
+            if (name.equals("Johnny")) {
+                return "Hello, my love!";
+            } else {
+                return String.format("Hello, %s!", name);
+            }
+
+        }
+        public void task5IO () {
+            System.out.println("Enter your name");
+            String name = Input.getStrInput();
+            System.out.println(task5(name));
         }
 
 
-        public int task6(int a) {
-        return Integer.parseInt(Integer.toBinaryString(a));
-    }
-    public void task6IO() {
-        System.out.println("Enter number");
-        int a = Input.getIntegerInput();
-        System.out.println(task6(a));
+        public int task6 ( int a){
+            return Integer.parseInt(Integer.toBinaryString(a));
+        }
+        public void task6IO () {
+            System.out.println("Enter number");
+            int a = Input.getIntegerInput();
+            System.out.println(task6(a));
 
 
-    }
-
-    public int task7(int h, int m, int s){
-        return (h * 360000) + (m * 60000) + (s * 1000);
-    }
-
-    public void task7IO(){
-        System.out.println("Enter the hours, minutes, and seconds to convert them to milliseconds");
-        int h = Input.getIntegerInput();
-        int m = Input.getIntegerInput();
-        int s = Input.getIntegerInput();
-        System.out.println(task7(h, m, s));
-    }
-
-    public int task8(int number){
-
-        int result;
-
-        if(number % 100 == 0){
-            result = number / 100;
-        }else{
-            result = (number / 100) + 1;
         }
 
-        return result;
-    }
-
-    public void task8IO(){
-        System.out.println("Enter any year to find out what century it is");
-        int number = Input.getIntegerInput();
-        System.out.println(task8(number));
-    }
-
-    public int task9(int x){
-
-        int result = x;
-
-        if(result > 0){
-            result = x - (x * 2);
+        public int task7 ( int h, int m, int s){
+            return (h * 360000) + (m * 60000) + (s * 1000);
         }
 
-        return result;
+        public void task7IO () {
+            System.out.println("Enter the hours, minutes, and seconds to convert them to milliseconds");
+            int h = Input.getIntegerInput();
+            int m = Input.getIntegerInput();
+            int s = Input.getIntegerInput();
+            System.out.println(task7(h, m, s));
+        }
+
+        public int task8 ( int number){
+
+            int result;
+
+            if (number % 100 == 0) {
+                result = number / 100;
+            } else {
+                result = (number / 100) + 1;
+            }
+
+            return result;
+        }
+
+        public void task8IO () {
+            System.out.println("Enter any year to find out what century it is");
+            int number = Input.getIntegerInput();
+            System.out.println(task8(number));
+        }
+
+        public int task9 ( int x){
+
+            int result = x;
+
+            if (result > 0) {
+                result = x - (x * 2);
+            }
+
+            return result;
+        }
+
+        public void task9IO () {
+            System.out.println("Enter any number and you will get the same but negative");
+            int x = Input.getIntegerInput();
+            System.out.println(task9(x));
+        }
     }
 
-    public void task9IO(){
-        System.out.println("Enter any number and you will get the same but negative");
-        int x = Input.getIntegerInput();
-        System.out.println(task9(x));
-    }
-}
