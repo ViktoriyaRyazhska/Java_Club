c0a13b76fed9b17bd059961704d7d80c334993e1
Roman Klymus <klimusr@gmail.com>
2021-07-23 14:27:30 +0300

    Select added

--- library.sql
+++ library.sql
@@ -48,7 +48,7 @@ create table  if not exists JOURNAL(ID int AUTO_INCREMENT PRIMARY KEY,
 -- INSERT
 insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Da Vinci Code,The', 20, '03:00:00');
 insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Harry Potter and the Deathly Hallows', 30,'5:00:00');
-insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Harry Potter and the Philosopher\'s Stone', 40, '4:00:00');
+insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Harry Potter and the Philosophers Stone', 40, '4:00:00');
 insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Harry Potter and the Order of the Phoenix', 5, '10:00:00');
 insert into book(TITLE, COPIES, AVERENGE_READING_TIME) values ('Fifty Shades of Grey', 0, '11:00:00');
 
@@ -69,6 +69,7 @@ insert into journal(USER_ID, BOOK_ID, DATE_OF_RENT, EXPECTED_RETURN_DATE, IS_BOO
 
 
 -- SELECT
+-- All users
 
 -- 1.	Get information about all books
 select * from book;
@@ -88,6 +89,36 @@ where authors.NAME='author name';
 select * 
 from book 
 where TITLE='book name';
+
+-- 5.	Get the most popular and the most unpopular books in selected period
+
+-- Reader
+
+-- 3.	Get his/her statistics (how many and how long books were been read, reading now)
+
+-- 4.	Get the most popular and the most unpopular books in selected period
+
+-- Manager
+
+-- 1.	Register book with copies
+
+-- 2.	Update book’ information
+
+-- 3.	Delete One copy/Book with all copies
+
+-- 4.	Give book to Reader
+
+-- 5.	Set title of book and display count of this Book’ copies with information about them (available/unavailable in Library)
+
+-- 6.	Get statistics by Reader (books which this user has read, is reading, how long he is our client)
+
+-- 7.	Get statistics by Book (general, by copies, average time of reading)
+
+-- 8.	Get statistics by Readers (average age, time of working with Library, average number of requests to Library in selected period)
+
+-- 9.	Get list of users who has not returned book in time with detailed information about them
+
+-- 10.	How many books were giving in selected period?
 
 
 
