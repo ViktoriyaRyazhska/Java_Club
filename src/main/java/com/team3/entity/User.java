d830afdcb2a140c4c537a2405d4527750ccbc288
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-29 10:57:55 +0300

    Add User, UserDao, UserDaoImpl

--- src/main/java/com/team3/entity/User.java
+++ src/main/java/com/team3/entity/User.java
@@ -1,7 +1,53 @@
 package com.team3.entity;
 
-import javax.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "user")
 public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "registrationDate")
+    private Date registrationDate;
+
+    @Column(name = "specialization")
+    private String specialization;
+
+    @ManyToMany(fetch=FetchType.EAGER,
+            cascade= {CascadeType.DETACH, CascadeType.MERGE,
+                    CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name="users_roles",
+            joinColumns=@JoinColumn(name="user_id"),
+            inverseJoinColumns=@JoinColumn(name="role_id"))
+    private Set<Role> roles;
 }
