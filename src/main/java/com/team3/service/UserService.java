bb3ebb04c85186121602665a24fe4fa4f9be27db
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-30 13:17:43 +0300

    add UserService

---
+++ src/main/java/com/team3/service/UserService.java
@@ -0,0 +1,13 @@
+package com.team3.service;
+
+import com.team3.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+
+    User getUserById(Long id);
+
+    List<User> getAllUsers();
+}
---
+++ src/main/java/com/team3/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.team3.service;
+
+import com.team3.dao.UserDao;
+import com.team3.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Transactional
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
