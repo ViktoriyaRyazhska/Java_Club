9b66888576b04bfeef197d900d10bc6f243a0ea8
Vitaliy622 <vitalikyarick@gmail.com>
2021-07-30 11:32:45 +0300

    add LibConfig

--- src/main/java/com/team3/config/LibConfig.java
+++ src/main/java/com/team3/config/LibConfig.java
@@ -3,12 +3,17 @@ package com.team3.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-
+@Configuration
+@ComponentScan("com.team3")
+@EnableWebMvc
 public class LibConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
