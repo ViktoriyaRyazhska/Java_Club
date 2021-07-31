82aad3638fba19b18048e2cfef36858698b02a6e
Roman Klymus <klimusr@gmail.com>
2021-07-31 22:50:35 +0300

    minor fix

--- src/main/java/com/softserve/team5/entity/Book.java
+++ src/main/java/com/softserve/team5/entity/Book.java
@@ -5,29 +5,29 @@ import javax.persistence.*;
 @Entity
 @Table(name = "book")
 public class Book {
+	
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Column(nullable = false)
+    private String title;
 
-	@Column(nullable = false)
-	private String title;
+    @Column(nullable = false)
+    private Long copies;
 
-	@Column(nullable = false)
-	private Long copies;
+    @Column(name = "AVERAGE_READING_TIME",nullable = false)
+    private Long averageReadingTime;
 
-	@Column(name = "AVERAGE_READING_TIME", nullable = false)
-	private Long averageReadingTime;
+    public Book() {
+    }
 
-	public Book() {
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	@Id
-	public Long getId() {
-		return id;
-	}
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
