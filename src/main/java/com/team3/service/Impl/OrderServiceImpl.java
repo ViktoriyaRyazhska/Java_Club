bd53b864c9b742bbe057633af625373efe684032
Vitaliy622 <vitalikyarick@gmail.com>
2021-08-06 16:46:36 +0300

    add findAll method

--- src/main/java/com/team3/service/Impl/OrderServiceImpl.java
+++ src/main/java/com/team3/service/Impl/OrderServiceImpl.java
@@ -1,11 +1,16 @@
 package com.team3.service.Impl;
 
+import com.team3.dao.OrderDao;
+import com.team3.entity.Order;
 import com.team3.service.OrderService;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private OrderDao orderDao;
     //TODO: add implementation to methods
     @Override
     public void getTheMostPopularBook() {
@@ -21,5 +26,29 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void getHowManyBooksWereGivingInSelectedPeriod() {
+    }
+
+    @Override
+    @Transactional
+    public void addOrder(Order order) {
+        orderDao.addOrder(order);
+    }
+
+    @Override
+    @Transactional
+    public void deleteOrder(Long id) {
+        orderDao.deleteOrder(id);
+    }
+
+    @Override
+    @Transactional
+    public Order findByOrderId(Long id) {
+        return orderDao.findOrderById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Order> findAllOrders() {
+       return orderDao.findAllOrders();
     }
 }
--- src/main/java/com/team3/service/OrderService.java
+++ src/main/java/com/team3/service/OrderService.java
@@ -1,11 +1,16 @@
 package com.team3.service;
 
+import com.team3.entity.Order;
+
+import java.util.List;
+
 public interface OrderService {
     void getTheMostPopularBook();
-
     void getTheMostUnpopularBook();
-
     void getHowManyBooksWereBeenReadByUser(Long id);
-
     void getHowManyBooksWereGivingInSelectedPeriod();
+    void addOrder(Order order);
+    void deleteOrder(Long id);
+    Order findByOrderId(Long id);
+    List<Order> findAllOrders();
 }
