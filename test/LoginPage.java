3b4806f819586f158c61878a0e08be065763eef6
VladGranat <simonphenex@gmail.com>
2021-08-10 14:19:45 +0300

    Test2.2

---
+++ test/LoginPage.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+
+        this.driver = driver;
+    }
+}
