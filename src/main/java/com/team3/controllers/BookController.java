a40df04277a3c603e7c1a4a394a29d156385b7f4
Vitaliy622 <vitalikyarick@gmail.com>
2021-09-16 16:06:19 +0300

    add search by author name and book title

--- src/main/java/com/team3/controllers/BookController.java
+++ src/main/java/com/team3/controllers/BookController.java
@@ -1,15 +1,23 @@
 package com.team3.controllers;
 
+import com.team3.entity.Book;
 import com.team3.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequestMapping(value = "/books")
 public class BookController {
 
     private final BookService bookService;
+
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
@@ -18,7 +26,14 @@ public class BookController {
     @GetMapping("/show")
     public String showAllBooks(Model model) {
         model.addAttribute("books", bookService.findAll());
-
         return "book/bookAll";
+    }
+
+    @GetMapping("/search")
+    public ModelAndView showBooksByAuthorName(@RequestParam(required = false, value = "name") String name) {
+        List<Book> list = bookService.findBooksByAuthor(name);
+        ModelAndView mav = new ModelAndView("book/bookByAuthor");
+        mav.addObject("list", list);
+        return mav;
     }
 }
--- src/main/java/com/team3/dao/Impl/BookDaoImpl.java
+++ src/main/java/com/team3/dao/Impl/BookDaoImpl.java
@@ -1,7 +1,6 @@
 package com.team3.dao.Impl;
 
 import com.team3.dao.BookDao;
-import com.team3.entity.Author;
 import com.team3.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,7 +8,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import java.util.List;
 
@@ -42,7 +40,7 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public List<Book> findAll() {
-        return sessionFactory.getCurrentSession().createQuery("select a from Book a join fetch a.authors",Book.class).getResultList();
+        return sessionFactory.getCurrentSession().createQuery("select a from Book a join fetch a.authors", Book.class).getResultList();
     }
 
     @Override
@@ -54,10 +52,18 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public List<Book> findBooksByAuthor(String name) {
-       /* Query q = sessionFactory.getCurrentSession()
-                .createQuery("select Book.bookId,Book.title,Book.description,Book.genre,Book.count,Author.name,Author.surname FROM Author ,BookAuthor ,Book WHERE Author.id=BookAuthor.author_id AND Book.bookId=BookAuthor.book_id AND Author.name=:name ");
+        Query q = sessionFactory.getCurrentSession().createQuery("SELECT b\n" +
+                "FROM Book b\n" +
+                "          JOIN fetch b.authors" +
+                "          JOIN fetch BookAuthor pa ON pa.bookId = b.bookId\n" +
+                "          JOIN fetch Author a ON a.id = pa.authorId\n" +
+                "WHERE a.surname LIKE :name\n" +
+                "   OR a.name LIKE :name " +
+                "   OR b.title LIKE :name");
+        // Query q = sessionFactory.getCurrentSession().createQuery("select b from Book b join fetch Author a where a.name like :name and BookAuthor.authorId =a.id ");
+        //Query q = sessionFactory.getCurrentSession().createQuery("select b from book b where  b.book_id IN(select pa.book_id from books_authors as pa,author as a where pa.author_id=a.author_id && (a.name= 'emily' or a.surname='asd'))\n");
+        // .createQuery("select Book.bookId,Book.title,Book.description,Book.genre,Book.count,Author.name,Author.surname FROM Author ,BookAuthor ,Book WHERE Author.id=BookAuthor.author_id AND Book.bookId=BookAuthor.book_id AND Author.name=:name ");
         q.setParameter("name", name);
-        return q.getResultList();*/
-        return null;
+        return q.getResultList();
     }
 }
