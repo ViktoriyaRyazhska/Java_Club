0f65d693ba4c37b9f07d8a48df412061b18684c3
Vitaliy622 <vitalikyarick@gmail.com>
2021-09-28 14:58:37 +0300

    minor changes in security

---
+++ src/main/java/com/team3/config/SpringSecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.team3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Configuration
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/team3/security/SecurityUser.java
@@ -0,0 +1,71 @@
+package com.team3.security;
+
+import com.team3.entity.Status;
+import com.team3.entity.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class SecurityUser implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+    private final boolean isActive;
+
+    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isAccountNonExpired();
+    }
+
+    public static UserDetails fromUser(User user) {
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getRoles());
+    }
+}
