12fc4e844e1201451df21c65b034c372dc9cb459
Vitaliy622 <vitalikyarick@gmail.com>
2021-10-16 20:02:19 +0300

    minor changes

--- src/main/java/com/team3/controllers/HelloController.java
+++ src/main/java/com/team3/controllers/HelloController.java
@@ -1,17 +1,11 @@
 package com.team3.controllers;
 
-import com.team3.entity.User;
 import com.team3.service.UserService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
